b7dea604c9399b98f71ecec999bff4af20c26642
Alison Sabuwala <alison@Alisons-MacBook-Air.local>
2022-10-10 12:47:44 -0400

    First Spring Boot commit. Generated by Spring Boot Initializr

---
+++ src/main/java/com/alison/CloudBackupServer/CloudBackupServerApplication.java
@@ -0,0 +1,13 @@
+package com.alison.CloudBackupServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBackupServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBackupServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alison/CloudBackupServer/CloudBackupServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alison.CloudBackupServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBackupServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
