220b8e5da877650767fe78d55c130e9d9070c419
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2022-12-27 17:44:44 -0500

    Added basic unit test and integration test

---
+++ src/test/java/com/alison/CloudBackupServer/DataControllerIntegrationTests.java
@@ -0,0 +1,23 @@
+package com.alison.CloudBackupServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DataControllerIntegrationTests {
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void getData() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/", String.class);
+        assertThat(response.getBody()).isEqualTo("My Home Page");
+    }
+}
